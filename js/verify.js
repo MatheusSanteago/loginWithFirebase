9fe8454cd8899ff29d580581af0b2c761e91a6cd
Matheus <matheussanteago@gmail.com>
2022-04-20 00:22:26 -0300

    day finalized

--- js/verify.js
+++ js/verify.js
@@ -2,12 +2,17 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 var inputData = document.querySelectorAll("input");
-var btnSubmit = document.querySelectorAll("button")[0];
+var btnSubmit = document.querySelectorAll("button");
+let bText = document.querySelectorAll("a");
 let email, password;
 
-btnSubmit.addEventListener("click", getInputData);
-var errorSpan = document.getElementById("sb");
+let form = document.querySelector("form");
+let text = document.querySelectorAll("h2");
+var icons = document.querySelectorAll("i");
 
+btnSubmit[0].addEventListener("click", getInputData);
+var errorSpan = document.getElementById("sb");
+var teste = document.getElementById("teste");
 //console.log(navigator.language);
 
 function getInputData(evt) {
@@ -21,17 +26,9 @@ function getInputData(evt) {
   }
 }
 
-function createUser(email, password) {
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((user) => {
-      console.log(user);
-    })
-    .catch((err) => console.log(err));
-}
-
 function login(email, password) {
+  text[0].innerHTML = "Realizando Login";
+  hideInput();
   firebase
     .auth()
     .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
@@ -39,12 +36,14 @@ function login(email, password) {
       firebase
         .auth()
         .signInWithEmailAndPassword(email, password)
-        .then(() => {
+        .then((user) => {
+          text[0].innerHTML =
+            "Parabéns seu login foi realizado! <span></span><br> Você será redirecionado para a página inicial";
+          console.log(user);
           console.log(firebase.auth().currentUser);
         })
         .catch((err) => {
-          errVerify(err)
-          console.log(err);
+          errVerify(err);
         });
     })
     .catch((err) => {
@@ -60,18 +59,56 @@ function logout() {
     .catch((err) => console.log(err));
 }
 
-function errVerify(err){
+function errVerify(err) {
   switch (err.code) {
     case "auth/too-many-requests":
-      errorSpan.innerHTML =
+      text[0].innerHTML =
         "Você já realizou muitas tentativas, tente mais tarde!";
+      reShow();
       break;
     case "auth/wrong-password":
-      errorSpan.innerHTML = "Você errou a senha, tente novamente";
+      text[0].innerHTML = "Você errou a senha, tente novamente";
+      reShow();
       break;
     case "auth/user-not-found":
-      errorSpan.innerHTML = "Usuário incorrecto ou inexistente.";
+      text[0].innerHTML = "Usuário incorrecto ou inexistente.";
+      reShow();
     default:
       break;
   }
 }
+
+function hideInput() {
+  btnSubmit[0].style.display = "none";
+  form.style.display = "none";
+  text[0].classList.add("aflip");
+  bText[0].style.display = "none";
+  animationLoading();
+}
+function reShow() {
+  icons[1].style.visibility = "hidden";
+  btnSubmit[0].style.display = "block";
+  form.style.display = "flex";
+  text[0].classList.remove("aflip");
+  bText[0].style.display = "block";
+}
+function animationLoading() {
+  icons[1].style.visibility = "visible";
+  icons[1].classList.add("faflip");
+  setTimeout(() => {
+    icons[1].style.visibility = "hidden";
+    icons[1].classList.remove("faflip");
+    //window.location.assign("https://www.youtube.com/watch?v=VwDDL06g604&ab_channel=DjStarSunglasses");
+  }, 2000);
+}
+
+var today = new Date();
+var date =
+  today.getDate() + " - " + today.getMonth() + " - " + today.getFullYear();
+var todayDate = new Date();
+var time =
+  todayDate.getHours() +
+  ":" +
+  todayDate.getMinutes() +
+  ":" +
+  todayDate.getSeconds();
